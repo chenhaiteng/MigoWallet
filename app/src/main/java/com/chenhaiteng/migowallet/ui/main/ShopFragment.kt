897db9f3f75eea1da310a5044d20c39dd047ef75
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-06-18 19:49:24 +0800

    Implement ShopFragment to present purchasable items.

---
+++ app/src/main/java/com/chenhaiteng/migowallet/ui/main/ShopFragment.kt
@@ -0,0 +1,80 @@
+package com.chenhaiteng.migowallet.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chenhaiteng.migowallet.R
+import com.chenhaiteng.migowallet.ui.main.placeholder.MockShop
+import kotlinx.android.synthetic.main.shop_fragment.view.*
+
+class ShopFragment : Fragment() {
+    private val shop: MockShop by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val root = inflater.inflate(R.layout.shop_fragment, container, false)
+        root.group_list.layoutManager = LinearLayoutManager(context)
+        root.group_list.adapter = GroupListAdapter(object: GroupListDataSource {
+            override fun numberOfGroups(groupList: GroupListAdapter): Int {
+                return 2
+            }
+
+            override fun numberOfItemsIn(groupList: GroupListAdapter, group: Int): Int {
+                return when(group) {
+                    0 -> {
+                        shop.numOfDayPass
+                    }
+                    1 -> {
+                        shop.numOfHourPass
+                    }
+                    else -> {
+                        0
+                    }
+                }
+            }
+
+            fun getHeaderItem(headerIndex: Pair<Int, Int>) : GroupListItem = GroupListItem(headerIndex, when(headerIndex.first) {
+                0 -> "Day Pass".toUpperCase()
+                1 -> "Hour Pass".toUpperCase()
+                else -> "!!!Incorrect Header!!!"
+            })
+
+            fun getPassItem(pathIndex: Pair<Int, Int>) : GroupListItem {
+                val item = when(pathIndex.first) {
+                    0 -> shop.availableDayPass()[pathIndex.second]
+                    1 -> shop.availableHourPass()[pathIndex.second]
+                    else -> null
+                }
+                return item?.let {
+                    val listItem = GroupListItem(pathIndex, "${it.title()}") {
+                        // TODO: add pass to cart.
+                    }
+                    listItem.description = "price \$${it.price}"
+                    listItem.actionTitle = R.string.buy_button
+                    listItem
+                } ?: run {
+                    GroupListItem(pathIndex, "Unknown Pass")
+                }
+            }
+
+            override fun getItem(
+                groupList: GroupListAdapter,
+                atIndex: Pair<Int, Int>
+            ): GroupListItem {
+                return when(atIndex.second) {
+                    -1 -> getHeaderItem(atIndex)
+                    else -> getPassItem(atIndex)
+                }
+            }
+        }, null)
+        return root
+    }
+
+    companion object {
+        fun newShop() = ShopFragment()
+    }
+}
